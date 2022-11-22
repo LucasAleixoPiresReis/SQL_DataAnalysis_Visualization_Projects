19683240209702998791610e04657ee1aca43624
Lucas Aleixo <117869039+LucasAleixoPiresReis@users.noreply.github.com>
2022-11-22 10:53:04 -0300

    Update Análise de preço de venda de notebooks.sql

--- Notebook_price_analysis/Análise de preço de venda de notebooks.sql
+++ Notebook_price_analysis/Análise de preço de venda de notebooks.sql
@@ -6,7 +6,7 @@
  
 "Vários fatores diferentes podem afetar os preços dos notebooks. Esses fatores incluem a marca do computador e o número de opções e complementos incluídos no pacote de computador. Além disso, a quantidade de memória e a velocidade do processador também podem afetar os preços. Embora menos comuns, alguns consumidores gastam dinheiro adicional para comprar um computador com base no "visual" e design gerais do sistema. Em muitos casos, os computadores de marca são mais caros do que as versões genéricas. Esse aumento de preço geralmente tem mais a ver com o reconhecimento de nomes do que com qualquer superioridade real do produto. Uma grande diferença entre a marca de nome e os sistemas genéricos é que, na maioria dos casos, os computadores de marca de nome oferecem melhores garantias do que as versões genéricas. Ter a opção de devolver um computador que está com defeito é muitas vezes um incentivo suficiente para incentivar muitos consumidores a gastar mais dinheiro. A funcionalidade é um fator importante na determinação dos preços dos computadores portáteis. Um computador com mais memória geralmente funciona melhor por mais tempo do que um computador com menos memória. Além disso, o espaço no disco rígido também é crucial, e o tamanho do disco rígido geralmente afeta os preços."
 
-**Referência:** https://www.easytechjunkie.com/what-factors-affect-desktop-computer-prices.htm#comments
+*Referência:* https://www.easytechjunkie.com/what-factors-affect-desktop-computer-prices.htm#comments
 
 > Análise:
   * Declaração de problema: Entenda o preço de venda dos notebooks e suas especificações técnicas.
